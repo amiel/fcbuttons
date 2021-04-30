a9228e9ce19f1cd460ea0c86e65029395f8b8693
Amiel Martin <amiel.martin@gmail.com>
2021-04-30 13:19:47 -0800

    Redo music operation if connection had failed

--- src/music.rs
+++ src/music.rs
@@ -13,18 +13,15 @@ pub fn new_client() -> anyhow::Result<MusicClient> {
 
 impl MusicClient {
     pub fn stop(&mut self) -> anyhow::Result<()> {
-        let result = self.conn.stop();
-        self.handle_errors(result)
+        self.handle_errors(|conn| conn.stop())
     }
 
     pub fn next(&mut self) -> anyhow::Result<()> {
-        let result = self.conn.next();
-        self.handle_errors(result)
+        self.handle_errors(|conn| conn.next())
     }
 
     pub fn prev(&mut self) -> anyhow::Result<()> {
-        let result = self.conn.prev();
-        self.handle_errors(result)
+        self.handle_errors(|conn| conn.prev())
     }
 
     // pub fn status(&mut self) -> Result<mpd::Status, mpd::error::Error> {
@@ -32,32 +29,30 @@ impl MusicClient {
     // }
 
     pub fn play_playlist(&mut self, name: &String) -> anyhow::Result<()> {
-        let result = self.conn.pause(true);
-        self.handle_errors(result)?;
-
-        let result = self.conn.clear();
-        self.handle_errors(result)?;
-
-        let result = self.conn.load(name, ..);
-        self.handle_errors(result)?;
-
-        let result = self.conn.play();
-        self.handle_errors(result)
+        self.handle_errors(|conn| {
+            conn.pause(true)?;
+            conn.clear()?;
+            conn.load(name, ..)?;
+            conn.play()
+        })
     }
 
     pub fn playlists(&mut self) -> anyhow::Result<Vec<mpd::Playlist>> {
-        // Result<Vec<mpd::Playlist>, mpd::error::Error> {
-        let result = self.conn.playlists();
-        self.handle_errors(result)
+        self.handle_errors(|conn| conn.playlists())
     }
 
-    fn handle_errors<T>(&mut self, result: mpd::error::Result<T>) -> anyhow::Result<T> {
+    fn handle_errors<F, T>(&mut self, f: F) -> anyhow::Result<T>
+    where
+        F: Fn(&mut Client<std::net::TcpStream>) -> mpd::error::Result<T>,
+    {
+        let result = f(&mut self.conn);
+
         if let Err(error) = result {
             println!("MPD Error: {}", error);
             println!("Trying to reconnect");
             self.attempt_reconnect()?;
 
-            Err(anyhow::anyhow!("Handled MPD Error: {}, try again", error))
+            Ok(f(&mut self.conn)?)
         } else {
             Ok(result?)
         }
